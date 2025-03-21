af9137b20e5f3832ac3277e5b1eb36094e6f7d2e
rodrigogm-dev <rodrigogm.dev@gmail.com>
2025-03-21 13:28:01 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rodrigogm-dev/movie-search',
+  base: '/movie-search',
   plugins: [react()]
 })
